921c884657719cb9ca431417538bf26f6202671c
Jiawei9527 <200607810@student.georgianc.on.ca>
2024-12-08 13:01:07 -0500

    Finish scene loading script

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,4 +8,18 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+    // Method to load a scene additively
+    public void LoadSceneAdditively(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        //Pause the current scene
+        Time.timeScale = 0f;
+    }
+    // Example method to unload a scene
+    public void UnloadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+        // Resume the current scene after unloading the additive scene
+        Time.timeScale = 1f;
+    }
 }
